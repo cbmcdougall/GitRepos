95c635fd5c89debae835d5c7a5025ea0a00fa31b
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-10 14:38:08 +0000

    fix API call triggering on form value change

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,26 @@ import React, { useState } from "react";
 import { Repositories } from "./components";
 
 export const App = () => {
+    const [formData, setFormData] = useState("");
     const [username, setUsername] = useState("");
     const [isSubmitted, setIsSubmitted] = useState(false);
 
     const handleInput = e => {
-        setUsername(e.target.value)
+        setFormData(e.target.value)
     }
     const handleFormSubmit = e => {
         e.preventDefault();
+        setUsername(formData);
         setIsSubmitted(true);
     }
 
     return (
         <>
             <form onSubmit={handleFormSubmit}>
-                <input type="text" name="username" id="username" onChange={handleInput} value={username} />
+                <input type="text" name="username" id="username" onChange={handleInput} value={formData} />
                 <button type="submit">Submit</button>
             </form>
             {isSubmitted && <Repositories username={username} />}
         </>
     )
 }
-
-// https://api.github.com/users/USERNAME/repos
-
-// name: "", commits_url: url, forks: #, language: "", stargazers_count: #, open_issues: #, watchers: #
