53c493909d197c68d7ba4c6a5b76797078a62091
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-10 16:59:39 +0000

    add styling

--- src/components/Repositories/index.jsx
+++ src/components/Repositories/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import { Repository } from '..';
+import './style.css'
 
 export const Repositories = ({ username }) => {
     const [repoData, setRepoData] = useState([]);
@@ -15,12 +16,14 @@ export const Repositories = ({ username }) => {
             }
             // const { data } = await axios.get(`https://api.github.com/users/${username}/repos`, options);
             // Mock data to prevent api limit rate
-            // name: "", commits_url: url, forks: #, language: "", stargazers_count: #, open_issues: #, watchers: #
-            console.log("API Fetch!");
             const data = [
-                { name: "Test Repo 1", commits_url: "", forks: 2, language: "JavaScript", stargazers_count: 1, open_issues: 3, watchers: 5 },
-                { name: "Test Repo 2", commits_url: "", forks: 1, language: "JavaScript", stargazers_count: 0, open_issues: 2, watchers: 0 },
-                { name: "Test Repo 3", commits_url: "", forks: 0, language: "JavaScript", stargazers_count: 3, open_issues: 0, watchers: 10 }
+                { name: "Test Repo", commits_url: "", forks: 2, language: "JavaScript", stargazers_count: 1, open_issues: 3, watchers: 5 },
+                { name: "Test Repo", commits_url: "", forks: 2, language: "JavaScript", stargazers_count: 1, open_issues: 3, watchers: 5 },
+                { name: "Test Repo", commits_url: "", forks: 2, language: "JavaScript", stargazers_count: 1, open_issues: 3, watchers: 5 },
+                { name: "Test Repo", commits_url: "", forks: 2, language: "JavaScript", stargazers_count: 1, open_issues: 3, watchers: 5 },
+                { name: "Test Repo", commits_url: "", forks: 2, language: "JavaScript", stargazers_count: 1, open_issues: 3, watchers: 5 },
+                { name: "fp_study_notes_connecting_APIs_to_databases", commits_url: "", forks: 1, language: "JavaScript", stargazers_count: 0, open_issues: 2, watchers: 0 },
+                { name: "Test Repo", commits_url: "", forks: 0, language: "JavaScript", stargazers_count: 3, open_issues: 0, watchers: 10 }
             ]
             if (cancelRequest) return;
             setRepoData(data);
@@ -34,7 +37,7 @@ export const Repositories = ({ username }) => {
     const renderRepo = () => repoData.map((r, i) => <Repository key={i} data={r} />)
 
     return (
-        <div>
+        <div className="repositories-container">
             {renderRepo()}
         </div>
     )
